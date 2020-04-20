9d07c409e0f21c8c1ab4ceffd189612aba5e7345
m-benrhabra <meriem.benrhabra1@gmail.com>
2020-04-20 22:26:06 +0000

    test N°1

--- function.js
+++ function.js
@@ -1,6 +1,6 @@
-let buttonn = document.querySelector(".click");
-let test = document.querySelector(".test");
-let question = document.querySelector(".questions");
+const buttonn = document.querySelector(".click");
+const test = document.querySelector(".test");
+const question = document.querySelector(".questions");
 
 buttonn.addEventListener("click", function () {
     test.style.display = "none";
